8ca7337ef37ad00ca9586499cc5a3e0ffa1cf2cb
alcl2000 <amy.555.lewis@gmail.com>
2023-05-04 16:02:28 +0000

    fix: no 405 error

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
@@ -7,23 +6,91 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
-
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-function SignInForm() {
-//   Add your component logic here
+const SignInForm = () => {
+    //  Component states
+    const [signInData, setSignInData] = useState({
+        username: "",
+        password: ""
+    });
+    const {username, password} = signInData;
+    const [errors, setErrors] = useState({});
+    const history = useHistory();
 
+    // Component logic 
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/login/", signInData);
+            history.push("/");
+        } catch (err) {
+            setErrors(err.response?.data);
+        };
+    };
+
+    // Render
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign in</h1>
-          {/* Add your form here */}
-
+            <h1 className={styles.Header}>sign in</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="username">
+                    <Form.Label className="d-none">Username</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Username" 
+                        name='username'
+                        className={styles.Input}
+                        onChange={handleChange}
+                        value={username}/>
+                </Form.Group>
+                {/*error handling*/}
+                {errors.username?.map((message, idx) => (
+                    <Alert key={idx} variant="warning">
+                        {message}
+                    </Alert>
+                ))}
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label className="d-none">Password</Form.Label>
+                    <Form.Control 
+                        className={styles.Input}
+                        type="password" 
+                        name='password'
+                        placeholder="Password" 
+                        onChange={handleChange}
+                        vaule={password}/>
+                </Form.Group>
+                {/*error handling*/}
+                {errors.password?.map((message, idx) => (
+                    <Alert key={idx} variant="warning">
+                        {message}
+                    </Alert>
+                ))}
+                <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                type="submit">
+                    Sign In 
+                </Button>
+                {/*error handling*/}
+                {errors.non_field_errors?.map((message, idx) => (
+                    <Alert key={idx} variant="warning" className="mt-3">
+                        {message}
+                    </Alert>
+                ))}
+            </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
