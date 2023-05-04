14e895adeeb662687d5e0e5b37539dad035f18f6
alcl2000 <amy.555.lewis@gmail.com>
2023-05-04 09:41:19 +0000

    add: communication with api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import styles from './App.module.css'
 import NavBar from './components/Navbar';
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from "react-router-dom";
+import './api/axiosDefaults' ;
 
 function App() {
   return (
